052b9b7ade0841d40e08a823aba24d02ab90f467
taaixsp <taai.xsp@hotmail.com>
2022-05-25 22:50:43 -0300

    Criação de componentes na Home

---
+++ src/pages/home/Home.tsx
@@ -0,0 +1,17 @@
+import React from "react";
+import './Home.css';
+
+
+function Home() {
+    return (
+        <>
+            <h1 className="title">Killmonger</h1>
+            <p className="font"> Lorem ipsum dolor sit amet consectetur, adipisicing elit. Culpa quos quas deserunt modi distinctio perspiciatis quis itaque sed, doloremque aspernatur, molestiae perferendis voluptas totam aperiam veritatis! Aut pariatur eveniet voluptatem?</p>
+
+            <img className="image" src="https://www.hqzona.com.br/wp-content/uploads/2021/05/20190607-black-panther-killmonger-empathetic-villain-e1551734669474.jpg" alt="Killmonger's picture"/>
+        </>
+    
+    )
+}
+
+export default Home;
