772eebc8a715b14c0ed4176c311cc0b7052c855a
taaixsp <taai.xsp@hotmail.com>
2022-05-25 22:56:51 -0300

    Implementação de titulo

--- src/pages/home/Home.tsx
+++ src/pages/home/Home.tsx
@@ -5,7 +5,7 @@ import './Home.css';
 function Home() {
     return (
         <>
-            <h1 className="title">Killmonger</h1>
+            <h1 className="titulo">Killmonger</h1>
             <p className="font"> Lorem ipsum dolor sit amet consectetur, adipisicing elit. Culpa quos quas deserunt modi distinctio perspiciatis quis itaque sed, doloremque aspernatur, molestiae perferendis voluptas totam aperiam veritatis! Aut pariatur eveniet voluptatem?</p>
 
             <img className="image" src="https://www.hqzona.com.br/wp-content/uploads/2021/05/20190607-black-panther-killmonger-empathetic-villain-e1551734669474.jpg" alt="Killmonger's picture"/>
