eec6147d1388d5a47049e4fd337f3516841a3e09
taaixsp <taai.xsp@hotmail.com>
2022-05-30 18:10:48 -0300

    Navbar, Home and Footer implemented

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,15 @@
 import React from 'react';
 import Home from './pages/home/Home';
 import './App.css';
+import Navbar from './components/estaticos/navbar/Navbar';
+import Footer from './components/estaticos/footer/Footer';
 
 function App() {
   return (
   <>
+  <Navbar />
     <Home />
+    <Footer />
   </>
   );
 }
--- src/pages/home/Home.tsx
+++ src/pages/home/Home.tsx
@@ -1,15 +1,30 @@
 import React from "react";
 import './Home.css';
+import {Grid, Box, Typography, Button} from '@material-ui/core';
 
 
 function Home() {
     return (
         <>
-            <h1 className="titulo">Killmonger</h1>
-            <p className="font"> Lorem ipsum dolor sit amet consectetur, adipisicing elit. Culpa quos quas deserunt modi distinctio perspiciatis quis itaque sed, doloremque aspernatur, molestiae perferendis voluptas totam aperiam veritatis! Aut pariatur eveniet voluptatem?</p>
-
-            <img className="image" src="https://www.hqzona.com.br/wp-content/uploads/2021/05/20190607-black-panther-killmonger-empathetic-villain-e1551734669474.jpg" alt="Killmonger's picture"/>
-        </>
+            <Grid container direction="row" justifyContent="center" alignItems="center" style={{ backgroundColor: "#3F51B5" }}>
+                <Grid alignItems="center" item xs={6}>
+                    <Box paddingX={20} >
+                        <Typography variant="h3" gutterBottom color="textPrimary" component="h3" align="center" style={{ color: "white", fontWeight: "bold" }}>Seja bem vindo(a)!</Typography>
+                        <Typography variant="h5" gutterBottom color="textPrimary" component="h5" align="center" style={{ color: "white", fontWeight: "bold" }}>expresse aqui os seus pensamentos e opiniões!</Typography>
+                    </Box>
+                    <Box display="flex" justifyContent="center">
+                        <Box marginRight={1}>
+                        </Box>
+                        <Button variant="outlined" style={{ borderColor: "white", backgroundColor: "#3F51B5", color: "white" }}>Ver Postagens</Button>
+                    </Box>
+                </Grid>
+                <Grid item xs={6} >
+                    <img src="https://i.imgur.com/H88yIo2.png" alt="" width="500px" height="500px" />
+                </Grid>
+                <Grid xs={12} style={{ backgroundColor: "white" }}>
+                </Grid>
+            </Grid>
+     </>
     
     )
 }
