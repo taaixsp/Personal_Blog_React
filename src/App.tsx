4f32778c66bb7bb84cf53708c45988b0bdc8e7da
taaixsp <taai.xsp@hotmail.com>
2022-05-30 23:40:54 -0300

    React Router DOM implemented

--- src/App.tsx
+++ src/App.tsx
@@ -3,14 +3,23 @@ import Home from './pages/home/Home';
 import './App.css';
 import Navbar from './components/estaticos/navbar/Navbar';
 import Footer from './components/estaticos/footer/Footer';
+import {BrowserRouter as Router, Route, Routes} from 'react-router-dom';
+import Login from './pages/login/Login';
 
 function App() {
   return (
-  <>
-  <Navbar />
-    <Home />
+    <Router>
+    <Navbar />
+    <div style={{ minHeight: '100vh'}}>
+      <Routes>
+        <Route path='/' element={<Login />}/>
+        <Route path='/login' element={<Login />}/>
+        <Route path='/home' element={<Home/>}/>
+        {/*     <Route path='/cadastro' element={<CadastrarUsuario/>}/> */}
+      </Routes>
+    </div>
     <Footer />
-  </>
+  </Router>
   );
 }
 
